242d7ea962741b58019c8285396e2a2c84fb2bae
Adam M. Krajewski <54290107+amkrajewski@users.noreply.github.com>
2024-02-19 14:03:34 -0500

    - added natsorting to ALIGNN results

--- featurization/alignn-minimal.py
+++ featurization/alignn-minimal.py
@@ -1,4 +1,5 @@
 # *****************   ALIGNN    **********************
+from natsort import natsorted
 print('Importing MPDD-ALIGNN...', flush=True)
 from alignn import pretrained
 print('ALIGNN Running...', flush=True)
@@ -9,6 +10,7 @@ print('ALIGNN Done!', flush=True)
 
 # Order keys in order "name", "SIPFENN_*", "ALIGNN_*"
 finalResult = [{**{'name': e['name']}, **{k: e[k] for k in e if k.startswith('ALIGNN')}} for e in alignnResult]
+finalResult = natsorted(finalResult, key=lambda e: e['name'])
 print(finalResult, flush=True)
 outString = f'| {" | ".join(finalResult[0].keys())} |\n'
 outString += f'| {" | ".join(["---" for _ in finalResult[0].keys()])} |\n'
