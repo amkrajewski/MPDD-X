4117528969c89b069c843dc1c6f10e054de7c1a5
Adam M. Krajewski <54290107+amkrajewski@users.noreply.github.com>
2024-02-18 22:48:54 -0500

    - calculation printout improved

--- featurization/minimal.py
+++ featurization/minimal.py
@@ -1,8 +1,9 @@
+print('Importing pySIPFENN...', flush=True)
 import pysipfenn
-from alignn import pretrained
 import numpy as np
 # ****************  pySIPFENN  **********************
 # Init
+print('\n\n\nStarting pySIPFENN Run', flush=True)
 c = pysipfenn.Calculator()
 # Get model and run from directory
 c.downloadModels('SIPFENN_Krajewski2022_NN30')
@@ -14,13 +15,17 @@ np.save('descriptorData.npy', resultArray)
 c.writeDescriptorsToCSV('KS2022', 'descriptorData.csv')
 sipfennResult = c.get_resultDictsWithNames()
 c.writeResultsToCSV('results.csv')
-print('pySIPFENN Done!')
+del c
+print('pySIPFENN Done!\n\n\n', flush=True)
 
 # *****************   ALIGNN    **********************
-print('ALIGNN Running...')
+print('Importing MPDD-ALIGNN...', flush=True)
+from alignn import pretrained
+print('ALIGNN Running...', flush=True)
 pretrained.download_default_models()
+print('ALIGNN Models Downloaded!', flush=True)
 alignnResult = pretrained.run_models_from_directory('structures', mode='serial')
-print('ALIGNN Done!')
+print('ALIGNN Done!', flush=True)
 
 # *********   Merge and write to file    **************
 # Match results based on the `name` field
@@ -33,7 +38,7 @@ for sr in sipfennResult:
 
 # Order keys in order "name", "SIPFENN_*", "ALIGNN_*"
 finalResult = [{**{'name': e['name']}, **{k: e[k] for k in e if k.startswith('SIPFENN')} , **{k: e[k] for k in e if k.startswith('ALIGNN')}} for e in finalResult]
-
+print(finalResult, flush=True)
 outString = f'| {" | ".join(finalResult[0].keys())} |\n'
 outString += f'| {" | ".join(["---" for _ in finalResult[0].keys()])} |\n'
 for e in finalResult:
@@ -42,4 +47,6 @@ for e in finalResult:
 print(outString)
 with open('response.md', 'w') as f:
     f.write(outString)
+
+print('\n\n\nAll Done!')
 
