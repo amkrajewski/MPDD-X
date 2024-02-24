0266c22b1915cc4e359e8eb54707fee3237a66c4
Adam M. Krajewski <54290107+amkrajewski@users.noreply.github.com>
2024-02-23 22:45:14 -0500

    - improve structure check printout

--- utils/structcheck.py
+++ utils/structcheck.py
@@ -12,13 +12,13 @@ for file in files:
     structures.append(Structure.from_file(f'structures/{file}'))
     names.append(file)
 print(f'Found {len(structures)} structures.\nPerforming a quick analysis.', flush=True)
-outString = "Structure Record:"
+outString = "Structure Record:\n"
 outString += f'| Name | Formula | Crytal Family | Space Group | Volume per Atom |\n'
 outString += f'| --- | --- | --- | --- | --- |\n'
 
 for s, n in zip(structures, names):
     sgA = SpacegroupAnalyzer(s)
-    outString += f'| {n} | {s.formula} | {sgA.get_crystal_system()} | {sgA.get_space_group_symbol()} | {s.volume / s.num_sites:.3f} |'
+    outString += f'| {n} | {s.formula} | {sgA.get_crystal_system()} | {sgA.get_space_group_symbol()} | {s.volume / s.num_sites:.3f} |\n'
 print(outString)
 with open('structureList.md', 'w') as f:
     f.write(outString)
